b875132e60430dd4b5350065e6a308579c7d8080
Temirlan Mukashev <tim.mukashev11gmail.com>
2025-01-13 15:39:32 +0500

    inital commit

---
+++ src/main/java/kz/solva/testtask/solva_test_app/SolvaTestAppApplication.java
@@ -0,0 +1,13 @@
+package kz.solva.testtask.solva_test_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvaTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvaTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/solva/testtask/solva_test_app/SolvaTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.solva.testtask.solva_test_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvaTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
